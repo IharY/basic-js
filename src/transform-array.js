d93f00a1734d1fa55138a51329a8a5acd8107bd8
Igor <igor@gmail.com>
2022-10-18 00:32:16 +0300

    feat: extebded-repeater + transform-array - done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,87 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  // if (arr.length!=0){
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] === '--discard-next') {
+  //       if(i!=arr.length-1){
+  //         let result = arr.slice(0, i) + "," + arr.slice(i + 2);
+  //         if(result!='[]'){
+  //           let newArray = result.split(',');
+  //           return newArray;
+  //         }else {
+  //           return undefined;
+  //         }
+  //       } else{
+  //         // return undefined;
+  //       }
+          
+  //     } else if (arr[i] === '--discard-prev') {
+  //       if(i!=0){
+  //         let result = arr.slice(0, i - 1) + "," + arr.slice(i + 1);
+  //         if(result!=='[]'){
+  //           let newArray = result.split(',');
+  //           return newArray;
+  //         }else{
+  //           return undefined;
+  //         }
+          
+  //       }else{
+  //         // return undefined;
+  //       }
+  
+  //     } else if (arr[i] === '--double-next') {
+  //       if(i != arr.length-1){
+  //         let result = arr.slice(0, i) + "," + arr[i + 1] + "," + arr.slice(i + 1);
+  //         let newArray = result.split(',');
+  //         return newArray;
+  //       } else{
+  //         // return undefined;
+  //       }
+          
+  //     } else if (arr[i] === '--double-prev') {
+  //       if(i != 0){
+  //         let result = arr.slice(0, i) + "," + arr[i - 1] + "," + arr.slice(i + 1);
+  //         let newArray = result.split(',');
+  //         return newArray;
+  //       } else{
+  //         // return undefined;
+  //       } 
+  //     }
+  //   }
+  // } else{
+  //   return arr;
+  // }
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  const newArr = [...arr];
+  result = [];
+
+  
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === undefined) {
+      continue;
+    } else if (newArr[i] === "--discard-next") {
+      i += 2;
+      continue;
+    } else if (newArr[i] === "--discard-prev") {
+      if (newArr[i - 1] !== undefined) result.pop();
+    } else if (newArr[i] === "--double-next") {
+      if (newArr[i + 1] !== undefined) result.push(newArr[i + 1]);
+    } else if (newArr[i] === "--double-prev") {
+      if (newArr[i - 1] !== undefined) result.push(arr[i - 1]);
+    } else {
+      result.push(newArr[i]);
+    }
+
+    
+  
+  }
+  
+
+  return result;
+  
 }
 
 module.exports = {
